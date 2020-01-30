8e3efdaeb2da83e019b659fe3e54441590d041de
fuston05 <fuston05@yahoo.com>
2020-01-29 21:58:54 -0500

    initial commit. basic set up complete. dependencies installed.

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Form from './components/Form/Form';
+
+//styles
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello World</h1>
+        
+    </div>
+  );
+}
+
+export default App;
