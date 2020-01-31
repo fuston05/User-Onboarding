b9f6a30518790ae68ac22693a0115ed283047d50
fuston05 <fuston05@yahoo.com>
2020-01-30 22:18:10 -0500

    got basic form set up, error validating, still working on mvp

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Form from './components/Form/Form';
+import LogInForm from './components/Form/Form';
 
 //styles
 import './App.css';
@@ -7,8 +7,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
-        
+      <h1>Hello World</h1>
+      <LogInForm />
     </div>
   );
 }
--- user-onboarding/src/components/Form/Form.js
+++ user-onboarding/src/components/Form/Form.js
@@ -1,11 +1,103 @@
 import React from 'react';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
 
-const Form = () => {
+// bring in props from withFormik as an obj{}
+const LogInForm = ( { values, errors, touched, status } ) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className= 'formCont'>
 
-export default Form;
+      <Form>
+        <label>Name:
+          <Field 
+          name= 'name' 
+          placeholder= 'Name'
+          />
+        </label>
+
+        {/* Yup error handling */}
+        {touched.name && errors.name && <p className= 'errors'>{ errors.name }</p>}
+
+        <label>Email:
+          <Field 
+          name= 'email'
+          type= 'email' 
+          placeholder= 'Email'
+          />
+        </label>
+
+         {/* Yup error handling */}
+        {touched.email && errors.email && <p className= 'errors'>{ errors.email }</p> }
+
+        <label>Password:
+          <Field 
+          name= 'password'
+          type= 'password' 
+          placeholder= 'Password'
+          />
+        </label>
+
+         {/* Yup error handling */}
+        {touched.password && errors.password && <p className= 'errors'>{ errors.password }</p>}
+
+        <label>Terms of Service:
+          <Field 
+          check= {values.terms.toString()}
+          name= 'terms'
+          type= 'checkbox' 
+          />
+        </label>
+
+         {/* Yup error handling */}
+        {touched.terms && errors.terms && <p className= 'errors'>{ errors.terms }</p>}
+
+        <button 
+        type= 'submit'
+        >Submit</button>
+      </Form>
+
+      {/* map over data and display here */}
+
+    </div> /**end formCont */
+  )
+}//end LogInForm
+
+const FormikLogInForm = withFormik({
+  // mapPropsToValues
+  // form values go here
+  // obj{} as props
+  mapPropsToValues({ name, email, password, terms }) {
+    return {// returns an obj
+      name: name || '',
+      email: email || '',
+      password: password || '',
+      terms: terms || false
+    }
+  },
+  //validationSchema
+  //'shape' is an obj
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required(),
+    email: Yup.string().required(),
+    password: Yup.string().required(),
+    terms: Yup.bool().required()
+  }),
+  //handleSubmit
+  // props: form values, {formikBag destructured methods}
+  handleSubmit(values, {resetForm, setStatus}){
+    console.log('submitting', values);
+    //axios
+    axios
+    .post()
+    .then()
+    .catch(err => {
+      console.log(err.response);
+    })
+  }
+
+
+
+})(LogInForm);
+
+export default FormikLogInForm;
